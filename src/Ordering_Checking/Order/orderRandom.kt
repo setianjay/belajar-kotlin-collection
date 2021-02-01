2c515cf9295ad658f53912a81dd89f2364752a54
setianjay <hari.setiaji2000@gmail.com>
2021-02-01 10:15:37 +0700

    Ordering Random

---
+++ src/Ordering_Checking/Order/orderRandom.kt
@@ -0,0 +1,11 @@
+package Ordering_Checking.Order
+
+fun main() {
+    val listNumbers = (1..10).toList()
+
+    println(listNumbers.shuffled())
+    println(listNumbers.shuffled())
+    println(listNumbers.shuffled())
+    println(listNumbers.shuffled())
+    println(listNumbers.shuffled())
+}
