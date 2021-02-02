866c9f52b3ef82cf603bc382ddcf60cf9fabba77
setianjay <hari.setiaji2000@gmail.com>
2021-02-02 13:10:57 +0700

    Map Spesific Operation

---
+++ src/Map/mapSpesificOperation.kt
@@ -0,0 +1,15 @@
+package Map
+
+fun main() {
+    val map = mapOf("a" to "Hari","b" to "Aprilia","c" to "Setiaji")
+
+    println(map.get("b"))
+    println(map.getValue("b"))
+    println(map.getOrDefault("d","Tidak ada"))
+    println(map.getOrElse("d",{"Tidak ada"}))
+
+    println(map.filter { it.value.count() > 5 })
+    println(map.filterKeys { keys -> keys != "a" })
+    println(map.filterValues { value -> value == "Aprilia" })
+
+}
